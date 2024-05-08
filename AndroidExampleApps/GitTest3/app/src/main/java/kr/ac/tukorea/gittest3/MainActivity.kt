91a9171a8a8ee544cb22071fe95d036d5cd4af6d
HyunBin0903 <hhb0664@naver.com>
2024-05-08 15:08:18 +0900

    로그인폼 FramLayout 기초틀

--- AndroidExampleApps/GitTest3/app/src/main/java/kr/ac/tukorea/gittest3/MainActivity.kt
+++ AndroidExampleApps/GitTest3/app/src/main/java/kr/ac/tukorea/gittest3/MainActivity.kt
@@ -1,15 +1,44 @@
 package kr.ac.tukorea.gittest3
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var layout_red: View
+    private lateinit var layout_green: View
+    private lateinit var layout_blue: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        InitializeView()
+    }
+
+    private fun InitializeView() {
+        layout_red = findViewById(R.id.Linear_Red)
+        layout_blue = findViewById(R.id.Linear_blue)
+        layout_green = findViewById(R.id.Linear_green)
+
+        findViewById<View>(R.id.btn_red).setOnClickListener(this)
+        findViewById<View>(R.id.btn_blue).setOnClickListener(this)
+        findViewById<View>(R.id.btn_green).setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        layout_red.visibility = View.INVISIBLE
+        layout_blue.visibility = View.INVISIBLE
+        layout_green.visibility = View.INVISIBLE
+
+        when (view.id) {
+            R.id.btn_red -> layout_red.visibility = View.VISIBLE
+            R.id.btn_blue -> layout_blue.visibility = View.VISIBLE
+            R.id.btn_green -> layout_green.visibility = View.VISIBLE
+        }
     }
 }
