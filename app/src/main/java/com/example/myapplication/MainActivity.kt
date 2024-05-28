fc610d43aeab83e58939a95b388daa6e893c6e84
HyunBin0903 <hhb0664@naver.com>
2024-05-28 21:49:45 +0900

    dasfsaf

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.database.database
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.messaging
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-        myRef.setValue("Hello, Firebase")
+        var txt = findViewById<TextView>(R.id.회원가입)
+        txt.setOnClickListener{
+            var intent = Intent(applicationContext, NewAccountPage::class.java)
+            startActivity(intent)
+        }
+      //val database = Firebase.database
+      //  val myRef = database.getReference("message")
+      //  myRef.setValue("Hello, Firebase")
+
     }
 }
