9b4c83329c5a40e2f67715adbe9def5d7bcd5ffe
HyunBin0903 <hhb0664@naver.com>
2024-05-30 10:05:51 +0900

    kakaobug아직 못고침

---
+++ app/src/main/java/com/kakao/sdk/auth/AuthCodeHandlerActivity/AuthCodeHandlerActivity.kt
@@ -0,0 +1,21 @@
+package com.kakao.sdk.auth.AuthCodeHandlerActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
+
+class AuthCodeHandlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_auth_code_handler)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
