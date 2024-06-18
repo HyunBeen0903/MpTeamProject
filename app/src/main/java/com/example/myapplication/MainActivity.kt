f116fa71cefd67fc0ed7eff871597b5899fa0152
HyunBin0903 <hhb0664@naver.com>
2024-06-18 15:19:29 +0900

    기본것에 카카오톡 api적용추가 및 홈화면 이동 기능 추가

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,23 +2,26 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.Firebase
-import com.google.firebase.database.database
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.messaging
+
 //Kangjihun Branch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var txt = findViewById<TextView>(R.id.회원가입)
+        var home = findViewById<Button>(R.id.홈화면이동)
         txt.setOnClickListener{
             var intent = Intent(applicationContext, NewAccountPage::class.java)
             startActivity(intent)
         }
+        home.setOnClickListener{
+            var intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
       //val database = Firebase.database
       //  val myRef = database.getReference("message")
       //  myRef.setValue("Hello, Firebase")
