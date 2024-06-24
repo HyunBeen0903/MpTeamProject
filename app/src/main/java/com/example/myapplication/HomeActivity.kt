f0e13ab0f55e98e87b2821c09a965774b6cb516f
HyunBin0903 <hhb0664@naver.com>
2024-06-25 03:19:46 +0900

    코스 이름 저장 시키는 코드 업데이트

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,8 +15,10 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         var hobit = findViewById<Button>(R.id.취향)
         var course = findViewById<LinearLayout>(R.id.등대)
+        var coursename = findViewById<TextView>(R.id.장소이름)
         course.setOnClickListener{
             var intent = Intent(applicationContext, CourseActivity::class.java)
+            intent.putExtra("coursename", coursename.text.toString() )
             startActivity(intent)
         }
         hobit.setOnClickListener{
