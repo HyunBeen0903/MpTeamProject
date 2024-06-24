bedb0826f36aec411680c4a509ca6b4e9c945ac2
givemefox <jhkang02000@gmail.com>
2024-06-24 16:47:14 +0900

    지도 수정

--- app/src/main/java/com/example/myapplication/UserCourse.kt
+++ app/src/main/java/com/example/myapplication/UserCourse.kt
@@ -5,5 +5,6 @@ import com.google.firebase.firestore.GeoPoint
 
 data class UserCourse(
     val name: String = "",
-    val points: List<GeoPoint> = emptyList()
+    val points: List<GeoPoint> = emptyList(),
+    val placeIds: List<String> = emptyList() // 장소 ID 목록 추가
 )
